1cb35298ce2e77416dcbfdfa1eaaf176ff0b9cf2
hibrid <samuelnmeje@gmail.com>
2020-10-29 16:15:18 +0100

    update image sources

--- Translator.vue
+++ Translator.vue
@@ -13,10 +13,10 @@
             <a :title="item.title"
                href="#">
                     <span class="language-item">
-                      <!--  <img :alt="item.alt" :src="`https://cdn.jsdelivr.net/npm/vue-gtranslate/img/flags/_${item.title}.png`"
-                             class="flag"/>-->
-                          <img :alt="item.alt" :src="`img/flags/__${item.title}.png`"
-                               class="flag"/>
+                        <img :alt="item.alt" :src="`https://cdn.jsdelivr.net/npm/vue-gtranslate/img/flags/__${item.title}.png`"
+                             class="flag"/>
+                      <!--    <img :alt="item.alt" :src="`img/flags/__${item.title}.png`"
+                               class="flag"/>-->
 
                       <span class="language__text">{{ item.title }}</span>
                     </span>
